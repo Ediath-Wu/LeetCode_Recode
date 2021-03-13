65b9444db7a2ca8c40f5d7a58aef935a95225862
wujie462 <ediath462@gmail.com>
2021-03-13 23:13:50 +0800

    Update 6.py
    优化了一下(其实就是抄了一下别人的答案啦,但是思路都是一样的)

--- 6.py
+++ 6.py
@@ -1,36 +1,54 @@
-class Solution(object):
-    def convert(self, s, numRows):
-        """
-        :type s: str
-        :type numRows: int
-        :rtype: str
-        """
+'''将一个给定字符串 s 根据给定的行数 numRows ，以从上往下、从左到右进行 Z 字形排列。
 
-        all_list = [[] for i in range(numRows)]
-        turn = 1
-        num = 1
-        count_str = 0
-        while True:
-            if num >= 1 and num <= numRows:
-                all_list[num-1].append(s[count_str])
-                count_str += 1
-                if count_str == len(s):
-                    break
-                num += turn
-            else:
-                turn = -turn
-                num += turn
-                num += turn
+比如输入字符串为 "PAYPALISHIRING" 行数为 3 时，排列如下：
 
-        ret = []
+P   A   H   N
+A P L S I I G
+Y   I   R
+之后，你的输出需要从左往右逐行读取，产生出一个新的字符串，比如："PAHNAPLSIIGYIR"。
 
-        rstr=''
+请你实现这个将字符串进行指定行数变换的函数：
 
-        for i in range(numRows):
-            for j in range(len(all_list[i])):
-                rstr+=all_list[i][j]
-        return rstr
+string convert(string s, int numRows);
+ 
 
+示例 1：
 
-a = Solution()
-print(a.convert('paypalishiring', 3))
+输入：s = "PAYPALISHIRING", numRows = 3
+输出："PAHNAPLSIIGYIR"
+示例 2：
+输入：s = "PAYPALISHIRING", numRows = 4
+输出："PINALSIGYAHRPI"
+解释：
+P     I    N
+A   L S  I G
+Y A   H R
+P     I
+示例 3：
+
+输入：s = "A", numRows = 1
+输出："A"
+ 
+
+提示：
+
+1 <= s.length <= 1000
+s 由英文字母（小写和大写）、',' 和 '.' 组成
+1 <= numRows <= 1000'''
+
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows < 2: return s
+        res = ["" for _ in range(numRows)]
+        i, flag = 0, -1
+        for c in s:
+            res[i] += c
+            if i == 0 or i == numRows - 1: flag = -flag
+            i += flag
+        return "".join(res)
+# 思路:
+#     创建一个大的列表
+#     列表里面放上numRows个小的列表
+#     取出s的每一个字符
+#     像给蛋糕上涂奶油一样把字符放进对应的小列表
+#     最后合并输出
